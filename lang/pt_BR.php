42fa6f5c11c817ee24330cf11bd0acd8e1731fed
João Marcos Ferreira de Lia <joaomarcos-fl@hotmail.com>
2017-09-11 04:14:25 -0300

    Create pt_BR.php

---
+++ lang/pt_BR.php
@@ -0,0 +1,23 @@
+<?php
+$unban['18'] = "Pais:";
+//First menu
+$unban['1'] = "Todos os banidos:";
+$unban['2'] = "Todos os desbanidos:";
+$unban['3'] = "IP:";
+$unban['4'] = "Status:";
+$unban['5'] = "Banido";
+$unban['6'] = "Não Banido";
+//Priv Order
+$unban['30'] = "<u>Pagamento via <b>SMS:</b></u> <br />";
+$unban['31'] = "Serviço não disponivel no momento";
+$unban['32'] = "<u>Outros <b>Pagamento</b> Métodos:</u> <br />";
+$unban['33'] = "Selecione o periodo de tempo:<br/>";
+$unban['34'] = "Pagar";
+$unban['35'] = "EUR";
+//Main Page
+$unban['unban_mokejimai'] = "Se você quiser comprar um desbanimento, envie sms com texto: <font color='red'><b>%s %s </b></font>número <font color='red'><b>%d</b></font>preço <b>%s %s</b>";
+$unban['unban_vpsnet'] = "Se você quiser comprar um desbanimento, envie sms com texto: <font color='red'><b>%s %s </b></font>número <font color='red'><b>%d</b></font> preço <b>%s %s</b>";
+$unban['have_priv'] = "Você já é ADMIN e/ou VIP!";
+$unban['other_payments'] = "Outros metodos de pagamento:";
+$unban['other_payments_pay'] = "Pagar";
+?>
